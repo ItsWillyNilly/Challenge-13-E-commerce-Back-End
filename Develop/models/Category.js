8dea070e13f64dfa6a711db1a3d04c5e6cbbc8d3
William Lee <williamlee@Williams-MacBook-Pro.local>
2024-07-28 16:53:09 -0700

    filled out the models in the models folder

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,7 +7,29 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
+  // {
+  //   hooks: {
+  //     beforeCreate: async (newCategoryData) => {
+  //       newCategoryData.category_name = newCategoryData.category_name.trim();
+  //       return newCategoryData;
+  //     },
+  //     beforeUpdate: async (updatedCategoryData) => {
+  //       updatedCategoryData.category_name = updatedCategoryData.category_name.trim();
+  //       return updatedCategoryData;
+  //     },
+  //   },
+  // },
   {
     sequelize,
     timestamps: false,
